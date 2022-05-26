f24423ca8605a42017893e1eff1497cda65b5ab6
Zeus <79009766+Zenerproton@users.noreply.github.com>
2022-05-26 23:03:26 +0530

    database engine chnaged

--- herbhouse/settings.py
+++ herbhouse/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'herbhouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
